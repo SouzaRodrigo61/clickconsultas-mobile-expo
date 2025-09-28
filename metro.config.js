b9258551879672a24c61790af9c358dacbea67d3
Rodrigo Santos de Souza <souza.rodrigo61@gmail.com>
2025-09-28 18:20:55 -0300

    Fix Node.js version compatibility for EAS Build

--- metro.config.js
+++ metro.config.js
@@ -1,4 +1,9 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
 const { getDefaultConfig } = require('expo/metro-config');
 
-module.exports = getDefaultConfig(__dirname);
+const config = getDefaultConfig(__dirname);
+
+// Add resolver configuration to handle metro-cache issues
+config.resolver.platforms = ['ios', 'android', 'native', 'web'];
+
+module.exports = config;
